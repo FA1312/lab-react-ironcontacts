81ac062019ed37f9443e8ba54c08cf1312e25a79
FA1312 <faldrovandi83@gmail.com>
2022-02-04 18:22:24 +0100

    css is missing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import contactData from "./contacts.json";
+import { useState } from "react";
 
 function App() {
+  const [contacts, setContacts] = useState(contactData.slice(0, 5));
+
+  const addContact = () => {
+    const randomIndex = Math.floor(Math.random() * (contactData.length - 1));
+    const newContArr = [contactData[randomIndex], ...contacts];
+    if (!contacts.includes(contactData[randomIndex])) {
+      setContacts(newContArr);
+    } else {
+      addContact();
+    }
+  };
+
+  const sortPop = () => {
+    let sortedByPop = contacts.sort((a, b) => b.popularity - a.popularity);
+    setContacts((contacts) => [...sortedByPop]);
+  };
+
+  const sortName = () => {
+    let sortedByName = contacts.sort((a, b) => a.name.localeCompare(b.name));
+    setContacts((contacts) => [...sortedByName]);
+  };
+
+  const deleteContact = (contactId) => {
+    const filteredContacts = contacts.filter((contact) => {
+      return contact.id !== contactId;
+    });
+    setContacts((contacts) => [...filteredContacts]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="list">
+      <h2>IronContacts</h2>
+      <button className="add-random" onClick={addContact}>
+        Add Random Contact
+      </button>
+      <button className="sort-popularity" onClick={sortPop}>
+        Sort by popularity
+      </button>
+      <button className="sort-name" onClick={sortName}>
+        Sort by name
+      </button>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Action</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+        {contacts.map((contact) => {
+          return (
+            <tbody key={contact.id}>
+              <tr>
+                <td>
+                  <img src={contact.pictureUrl} alt={contact.name} />
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity.toFixed(2)}</td>
+                <td>
+                  <button
+                    className="delete-btn"
+                    onClick={() => deleteContact(contact.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+                <td>{contact.wonOscar && <p>🏆</p>}</td>
+                <td>{contact.wonEmmy && <p>🏆</p>}</td>
+              </tr>
+            </tbody>
+          );
+        })}
+      </table>
     </div>
   );
 }
